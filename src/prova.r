3bc0a5aed3ac0cb58a7421b58a8e23a8f27eff76
Pol Casacuberta <polcg10@gmail.com>
2023-02-18 19:46:18 +0100

    added total number of missing per variable

--- src/prova.r
+++ src/prova.r
@@ -4,8 +4,13 @@ db <- read.csv(paste0(dir,"/data/census-income.data"),header=F, sep=",", strip.w
 mean(is.na(db)) * 100
 num_question_marks <- sum(db == "?", na.rm = TRUE)
 total_cells = ncol(db) * nrow(db)
+
 # grepl returns whether that value is found or not
+col_missing <- colSums(sapply(db, function(x) grepl("\\?", x)))
+col_missing
+
 col_percents <- colSums(sapply(db, function(x) grepl("\\?", x)))/ nrow(db) * 100
 col_percents
+
 average_missing <- (sum(col_percents, na.rm = TRUE) / length(col_percents)) 
 average_missing
