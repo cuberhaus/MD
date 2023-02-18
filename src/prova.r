41aff408b69d3497d68a306166fdd30c8cefbce2
cuberhaus <polcg10@gmail.com>
2023-02-18 17:09:21 +0100

    This is a placeholder

--- src/prova.r
+++ src/prova.r
@@ -1,2 +1,4 @@
-db <- read.table("C:/Users/polcg/Desktop/census-income.data",header=F, sep=",")
+# Go to the folder where you have stored the MD repo before running the script
+dir <- getwd()
+db <- read.table(paste0(dir,"/data/census-income.data"),header=F, sep=",")
 mean(is.na(db)) * 100
