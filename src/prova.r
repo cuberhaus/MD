b42bf447f2fa0659388042368bc4130f3facc0f3
cuberhaus <polcg10@gmail.com>
2023-02-18 17:26:32 +0100

    This is a placeholder

--- src/prova.r
+++ src/prova.r
@@ -1,4 +1,6 @@
 # Go to the folder where you have stored the MD repo before running the script
 dir <- getwd()
-db <- read.table(paste0(dir,"/data/census-income.data"),header=F, sep=",")
+db <- read.csv(paste0(dir,"/data/census-income.data"),header=F, sep=",", strip.white = TRUE) # take out trailing whitespaces
 mean(is.na(db)) * 100
+num_question_marks <- sum(db == "?", na.rm = TRUE)
+total_cells = ncol(db) * nrow(db)
