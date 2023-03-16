6562e839462751043c61215f0fc6a2182de2de8b
cuberhaus <polcg10@gmail.com>
2023-03-16 23:32:41 +0100

    pca updated, moved to markdown to show plots

--- src/PCA.r
+++ src/PCA.r
@@ -1,24 +1,30 @@
 # Go to the MD project folder before running the script
 dir <- getwd()
 # Import preprocessed data
-db <- read.csv(paste0(dir,"/data/new_imputed_data.csv"),header=T, sep=",", strip.white = TRUE) # take out trailing whitespaces
+# db <- read.csv(paste0(dir,"/data/Preprocessed_data.csv"),header=T, sep=",", strip.white = TRUE) # take out trailing whitespaces
+
+# read dataframe back in from RDS file
+db <- readRDS(paste0(dir,"/data/Preprocessed_data_rds.rds"))
+
 # Check if there are any missing values, there should not be because they have been imputed, or removed
 sum(is.na(db))
+# get number of missing values by column
+na_counts <- colSums(is.na(db))
 
 objects()
-attributes(df)
+attributes(db)
 
-attach(df)
-names(df)
+attach(db)
+names(db)
 
 #is R understanding well my factor variables?
-sapply(df,class)
+sapply(db,class)
 
-numeriques<-which(sapply(df,is.numeric))
+numeriques<-which(sapply(db,is.numeric))
 numeriques
 
-dcon<-df[,numeriques]
-sapply(dcon,class)
+categorics<-which(sapply(db,is.factor))
+categorics
 
 # Load the dplyr package
 #install.packages("dplyr")
@@ -36,7 +42,7 @@ summary(pca)
 
 #write.table(data, file = "data/processed_data/one_hot_db.csv", sep = ",", row.names = FALSE)
 
-install.packages("corrplot")
+#install.packages("corrplot")
 
 # Load the corrplot package
 library(corrplot)
